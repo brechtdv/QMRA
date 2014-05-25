6f081cb7ebe68c1291fa55bd802431795b22a5a4
Brecht Devleesschauwer <Brecht.Devleesschauwer@UGent.be>
2014-05-26 01:05:58 +0300

    Introduce 'print.gof', fix 'print.avg' for 'drm'

--- R/print-methods.R
+++ R/print-methods.R
@@ -19,7 +19,11 @@ function(x, ...) {
   from <-
     c("count", "concentration", "presence/absence")[
       which(x$fun == c("ea_count", "ea_conc", "ea_presence"))]
-  cat("Exposure assessment from", from, "data\n\n")
+  if (length(from) != 0) {
+    cat("Exposure assessment from", from, "data\n\n")
+  } else {
+    cat("Dose-response assessment\n\n")
+  }
 
   ## model estimates
   cat("Individual model estimates:\n")
@@ -28,4 +32,23 @@ function(x, ...) {
   ## model estimates
   cat("\nModel average:\n")
   print(x$avg)
+}
+
+## Print Goodness of Fit
+print.gof <-
+function(x, ...) {
+  cat("Chi-square goodness of fit test\n\n")
+  cat("Saturated likelihood:",
+      x$L0[1], "on", x$L0[2], "degree(s) of freedom\n")
+  cat("    Model likelihood:",
+      x$L1[1], "on", x$L1[2], "degree(s) of freedom\n\n")
+
+  df <- x$L0[2] - x$L1[2]
+  chisq <- -2 * log(x$L1[1] / x$L0[1])
+  p     <- pchisq(chisq, df, lower.tail = FALSE)
+
+  out <- data.frame(chisq, df, p)
+  rownames(out) <- ""
+  colnames(out) <- c("ChiSq", "df", "Pr>ChiSq")
+  print(out)
 }
